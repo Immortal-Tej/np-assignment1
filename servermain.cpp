ad02a5c54473d31ca7e5e6394543943ca44ab4e7
Immortal-Tej <vesa22@student.bth.se>
2025-09-28 17:35:18 +0200

    Added protocol handshake

--- servermain.cpp
+++ servermain.cpp
@@ -1,37 +1,129 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-/* You will to add includes here */
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <sys/select.h>
 
-
-// Included to get the support library
 #include <calcLib.h>
 
-// Enable if you want debugging to be printed, see examble below.
-// Alternative, pass CFLAGS=-DDEBUG to make, make CFLAGS=-DDEBUG
 #define DEBUG
-
 
 using namespace std;
 
 
 int main(int argc, char *argv[]){
   
-  /*
-    Read first input, assumes <ip>:<port> syntax, convert into one string (Desthost) and one integer (port). 
-     Atm, works only on dotted notation, i.e. IPv4 and DNS. IPv6 does not work if its using ':'. 
-  */
   char delim[]=":";
   char *Desthost=strtok(argv[1],delim);
   char *Destport=strtok(NULL,delim);
-  // *Desthost now points to a sting holding whatever came before the delimiter, ':'.
-  // *Dstport points to whatever string came after the delimiter. 
 
-  /* Do magic */
   int port=atoi(Destport);
 #ifdef DEBUG  
   printf("Host %s, and port %d.\n",Desthost,port);
 #endif
 
+  initCalcLib();
 
+  struct addrinfo hints, *servinfo;
+  int sockfd;
+  
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
+  
+  int rv = getaddrinfo(Desthost, Destport, &hints, &servinfo);
+  if (rv != 0) {
+    printf("getaddrinfo error: %s\n", gai_strerror(rv));
+    return 1;
+  }
+  
+  sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+  if (sockfd == -1) {
+    printf("Socket creation failed\n");
+    freeaddrinfo(servinfo);
+    return 1;
+  }
+  
+  int yes = 1;
+  if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+    printf("setsockopt failed\n");
+    close(sockfd);
+    freeaddrinfo(servinfo);
+    return 1;
+  }
+  
+  if (bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen) == -1) {
+    printf("Bind failed\n");
+    close(sockfd);
+    freeaddrinfo(servinfo);
+    return 1;
+  }
+  
+  freeaddrinfo(servinfo);
+  
+  if (listen(sockfd, 5) == -1) {
+    printf("Listen failed\n");
+    close(sockfd);
+    return 1;
+  }
+  
+#ifdef DEBUG
+  printf("Server listening on %s:%d\n", Desthost, port);
+#endif
+
+  while (1) {
+    struct sockaddr_storage client_addr;
+    socklen_t addr_size = sizeof(client_addr);
+    
+    int clientfd = accept(sockfd, (struct sockaddr *)&client_addr, &addr_size);
+    if (clientfd == -1) {
+      continue;
+    }
+    
+    const char *protocol_msg = "TEXT TCP 1.0\n\n";
+    send(clientfd, protocol_msg, strlen(protocol_msg), 0);
+    
+    fd_set readfds;
+    struct timeval timeout;
+    
+    FD_ZERO(&readfds);
+    FD_SET(clientfd, &readfds);
+    timeout.tv_sec = 5;
+    timeout.tv_usec = 0;
+    
+    int select_result = select(clientfd + 1, &readfds, NULL, NULL, &timeout);
+    
+    if (select_result == 0) {
+      const char *timeout_msg = "ERROR TO\n";
+      send(clientfd, timeout_msg, strlen(timeout_msg), 0);
+      close(clientfd);
+      continue;
+    }
+    
+    if (select_result > 0) {
+      char buffer[256];
+      int bytes_received = recv(clientfd, buffer, sizeof(buffer) - 1, 0);
+      
+      if (bytes_received > 0) {
+        buffer[bytes_received] = '\0';
+        
+        if (strcmp(buffer, "OK\n") == 0) {
+          // For now, just close after handshake
+          close(clientfd);
+        } else {
+          close(clientfd);
+        }
+      } else {
+        close(clientfd);
+      }
+    } else {
+      close(clientfd);
+    }
+  }
+  
+  close(sockfd);
+  return 0;
 }
